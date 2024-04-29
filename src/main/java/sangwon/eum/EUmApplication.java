57423a0547c674f8a37bee9d40c3a2b1ad12a1ff
정상원 <topcircle@hanyang.ac.kr>
2024-04-29 13:18:49 +0900

    첫 커밋

---
+++ src/main/java/sangwon/eum/EUmApplication.java
@@ -0,0 +1,13 @@
+package sangwon.eum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EUmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EUmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sangwon/eum/EUmApplicationTests.java
@@ -0,0 +1,13 @@
+package sangwon.eum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
